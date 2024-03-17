5cd4c3fd9765f1eebc6d5e82d76a7ff2f46df745
Vikash <v1kash@proton.me>
2024-03-17 22:43:30 +0530

    Add 'authapi' to project

--- src/core/settings.py
+++ src/core/settings.py
@@ -26,9 +26,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework_simplejwt",
+    "rest_framework.authtoken",
     # custom apps
-    "restauth",
+    "authapi",
 ]
 
 MIDDLEWARE = [
@@ -113,6 +113,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# AUTH_USER_MODEL = "authapi.User"
 
 # REST FRAMEWORK CONFIGURATIONS
 
