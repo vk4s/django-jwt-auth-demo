05795233dad93f93458f07f15fab4ea25156aa63
Vikash <v1kash@proton.me>
2024-03-17 22:42:20 +0530

    Create authentication api app

---
+++ src/authapi/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# class User(AbstractUser):
+#     email = models.EmailField(unique=True)
+#     username = None
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+
+#     def __str__(self):
+#         return self.email
